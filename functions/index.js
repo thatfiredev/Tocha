aaabae630e1a29609b6fc011cc8d4b5839962385
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-12-17 13:55:14 +0200

    refactor: use async/await to read all the documents from the collection

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const lunr = require('lunr');
 
 exports.searchFirestore = functions.firestore
     .document('tocha_searches/{searchId}')
-    .onCreate((snap, context) => {
+    .onCreate(async (snap, context) => {
         // Obtain the request parameters
         const req = snap.data();
         const collectionName = req.collectionName;
@@ -14,39 +14,37 @@ exports.searchFirestore = functions.firestore
         const query = req.query;
         const queryRef = req.queryRef;
 
-        // Access the collection to be searched
-        const collection = admin.firestore().collection(collectionName);
-        return collection.get().then(function (querySnapshot) {
-            var documents = [];
-            var lunrIndex = lunr(function() {
-                if (queryRef) {
-                    this.ref(queryRef);
-                } else {
-                    this.ref('key');
-                }
-                for (var i in fields) {
-                    this.field(fields[i]);
-                }
-                querySnapshot.forEach(function (docSnapshot) {
-                    var snapshotData = docSnapshot.data();
-                    documents[docSnapshot.id] = docSnapshot.data();
-                    snapshotData.key = docSnapshot.id;
-                    this.add(snapshotData);
-                }, this);
-            });
-            const results = lunrIndex.search(query);
-            var response = [];
-            results.forEach(function(result) {
-                response.push({
-                    id: result.ref,
-                    score: result.score,
-                    data: documents[result.ref]
-                })
-            });
-            admin.firestore().collection("tocha_searches").doc(context.params.searchId)
-                .update({
-                    response: response,
-                    responseTimestamp: admin.firestore.FieldValue.serverTimestamp()
-                });
+        // Read all the documents form the collection to be searched
+        const querySnapshot = await admin.firestore().collection(collectionName).get();
+        var documents = [];
+        var lunrIndex = lunr(function() {
+            if (queryRef) {
+                this.ref(queryRef);
+            } else {
+                this.ref('key');
+            }
+            for (var i in fields) {
+                this.field(fields[i]);
+            }
+            querySnapshot.forEach(function (docSnapshot) {
+                var snapshotData = docSnapshot.data();
+                documents[docSnapshot.id] = docSnapshot.data();
+                snapshotData.key = docSnapshot.id;
+                this.add(snapshotData);
+            }, this);
         });
+        const results = lunrIndex.search(query);
+        var response = [];
+        results.forEach(function(result) {
+            response.push({
+                id: result.ref,
+                score: result.score,
+                data: documents[result.ref]
+            })
+        });
+        return admin.firestore().collection("tocha_searches").doc(context.params.searchId)
+            .update({
+                response: response,
+                responseTimestamp: admin.firestore.FieldValue.serverTimestamp()
+            });
     });
